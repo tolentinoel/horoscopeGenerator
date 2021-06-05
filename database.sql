aecd5310aca2c4edbcca7a1555aefc740e8dec86
tolentinoel <rckrleyn@gmail.com>
2021-06-05 13:43:07 -0700

    ongoing deployment, linked database.sql to heroku

--- database.sql
+++ database.sql
@@ -1,6 +1,3 @@
-CREATE DATABASE name_database;
-
---\c into name_database
 
 CREATE TABLE names (
     id SERIAL PRIMARY KEY NOT NULL,
@@ -12,9 +9,9 @@ CREATE TABLE names (
 );
 --  MOCK DATA TO TEST SQL AND FRONT END
 
-INSERT INTO names(id, firstname, gender, hexcode, sign)
-VALUES(DEFAULT, 'Kim', 'Female', 'f7daef', 'cancer'),
-      (DEFAULT, 'Ellaine', 'Female', 'caf000', 'taurus' );
+-- INSERT INTO names(id, firstname, gender, hexcode, sign)
+-- VALUES(DEFAULT, 'Kim', 'Female', 'f7daef', 'cancer'),
+--       (DEFAULT, 'Ellaine', 'Female', 'caf000', 'taurus' );
 
 
 
