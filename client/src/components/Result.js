883a333154a40f7b9e4d855751bd0feb507be592
tolentinoel <rckrleyn@gmail.com>
2021-09-14 19:21:23 -0700

    refactor code added component lifecycle to prevent multiple fetch

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -13,17 +13,8 @@ class Result extends React.Component {
     }
 
 
-    loadData = (props) => {
-        this.setState({loading: false});
-
-
-    }
-    isLoading = () => {
-        this.loadData(this.props)
-    }
-
-    colorName = (hex) => {
-        let hexcode = hex[0].replace(/[^a-zA-Z ]/, "")
+    componentDidMount() {
+        let hexcode = this.props.colorState[0].replace(/[^a-zA-Z ]/, "")
 
         fetch(`https://www.thecolorapi.com/id?hex=${hexcode}`, {
                 method:  'GET',
@@ -33,19 +24,22 @@ class Result extends React.Component {
                 })
                 .then(res => res.json())
                 .then(newData => {
-
                     this.setState({
                         nameColor: newData.name.value
                     })
                 })
-                this.colorOfTheDay(this.state.nameColor)
+                
+    }
 
-        }
+    loadData = (props) => {
+        this.setState({loading: false});
 
-        colorOfTheDay = (str) => {
-           return <p><strong>Your color of the Day:</strong><br/> {str}</p>
-        }
 
+    }
+    isLoading = () => {
+        this.loadData(this.props)
+    }
+      
 
 
     render(){
@@ -54,12 +48,18 @@ class Result extends React.Component {
 
 
                 <div>
+                    {/* {this.colorName(this.props.colorState)} */}
                     { Object.keys(this.state.data).length > 0 ?
                         <div id="result" className= "text-center my-3" style={{backgroundColor: `${this.props.colorState[0]}`}}>
-                            <h2>{this.props.nameState}</h2>
-                            <p>Sign: {this.props.zodiacState}</p>
-                            <p><strong>General advice:</strong><br/> {this.props.data}</p>
-                            {this.colorName(this.props.colorState)}
+                            <div>
+                                <h2>{this.props.nameState}</h2>
+                                <p>Sign: {this.props.zodiacState}</p>
+                                <p><strong>General advice:</strong><br/> {this.props.data}</p>
+                            </div>
+                            <div>
+                            <p><strong>Your color of the Day:</strong><br/>{this.state.nameColor}</p>
+
+                            </div>
                         </div>
                         :
                         <>
