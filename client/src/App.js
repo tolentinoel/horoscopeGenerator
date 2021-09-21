647ac8ce8411764d14638619bd3715c5e33681d2
tolentinoel <rckrleyn@gmail.com>
2021-09-21 14:48:42 -0700

    Change bg url, added filter

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import InputName from './components/InputName';
 function App() {
   return (
     <div className="App">
-
+      <div id="bg"><img src="https://i.pinimg.com/originals/2a/0e/32/2a0e32c7a0584b5928252d800837e348.jpg" alt="vector wallpaper"/></div>
       <InputName/>
 
     </div>
